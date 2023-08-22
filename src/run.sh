dcda7444f6fe9e422e7e23715565b06ac88c51db
Ben Goldman <benogoldman@gmail.com>
2023-08-21 20:27:20 -0400

    more finally

--- src/run.sh
+++ src/run.sh
@@ -4,7 +4,7 @@
 #SBATCH --account=thea
 #SBATCH --job-name=khmhd
 #SBATCH -c 32 # of cores per node
-#SBATCH -N 8  # of nodes
+#SBATCH -N 4  # of nodes
 #SBATCH --mem-per-cpu=5G      # The memory the job will use per cpu core
 #SBATCH --time=0-8:00         # The time the job will take to run in D-HH:MM
 #SBATCH --mail-type=END
@@ -15,7 +15,7 @@ module load anaconda
 source /burg/home/bog2101/.bashrc
 conda activate spectralDNS
 date
-mpiexec -n 256 python khmhd1.py
+mpiexec -n 128 python khmhd.py
 date
 
 # End of script
